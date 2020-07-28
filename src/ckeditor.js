983a795631cc19a28752e7e28dc014d4f66b008c
Ryan Whitmire <ryanw51@gmail.com>
2020-07-28 10:13:49 -0400

    update default image toolbar

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -91,12 +91,26 @@ ClassicEditor.defaultConfig = {
   },
   image: {
     toolbar: [
-      "imageStyle:full",
-      "imageStyle:side",
-      "|",
       "imageTextAlternative",
+      "|",
+      "imageStyle:alignLeft",
+      "imageStyle:full",
+      "imageStyle:alignRight",
+      "|",
+      "linkImage"
     ],
-  },
+
+    styles: [
+      // This option is equal to a situation where no style is applied.
+      "full",
+
+      // This represents an image aligned to the left.
+      "alignLeft",
+
+      // This represents an image aligned to the right.
+      "alignRight"
+    ]
+  }
   table: {
     contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
   },
